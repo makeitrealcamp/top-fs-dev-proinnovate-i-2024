aac6e9b83c898524c52c2b9cd8a59f32ab3e855c
JaquelineRocio <jaquelineramosvargas@gmail.com>
2024-09-03 10:46:50 -0500

    Feature/javascripting jaqueline ramos solve the challenges (#53)
    * feat: solution introduction
    
    * feat: solution variables
    
    * feat: solution strings
    
    * feat: solution string-length
    
    * feat: solution revising-length
    
    * feat: solution numbers
    
    * feat: solution rounding-numbers
    
    * feat: solution number-to-string
    
    * feat: solution if-statement
    
    * feat: solution for-loop
    
    * feat: solution arrays
    
    * feat: solution array-filtering
    
    * feat: solution accessing-array-values
    
    * feat: solution accessing-array-values
    
    * feat: solution objects
    
    * feat: solution object-properties
    
    * feat: solution object-keys
    
    * feat: solution functions
    
    * feat: solution function-arguments
    
    * feat: solution scope

---
+++ nodeschool/javascripting/jaqueline-ramos/if-statement.js
@@ -0,0 +1,7 @@
+let fruit = "orange";
+let fruitLength = fruit.length;
+if (fruitLength > 5) {
+  console.log("The fruit name has more than five characters.");
+} else {
+  console.log("The fruit name has five characters or less.");
+}
