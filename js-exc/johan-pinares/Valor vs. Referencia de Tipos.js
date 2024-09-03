8fdc1ad16575d1d671c5efd803dc4f83cc9d45da
Johanirus <johan_kyurem@hotmail.com>
2024-09-03 09:54:05 -0500

    fix: solution values vs type reference

---
+++ js-exc/johan-pinares/Valor vs. Referencia de Tipos.js
@@ -0,0 +1,18 @@
+function checkEquals(arr1,arr2) {
+  if (arr1.length !== arr2.length){
+    return false;
+  } else {
+    for(let i=0 ; i< arr1.length; i++){
+      if(arr1[i] !== arr2[i]){
+        return false;
+      }
+    }
+    return true;
+  }
+  }
+console.log(checkEquals([1,2], [1,3]))
+console.log(checkEquals([1,2], [1,2]))
+console.log(checkEquals([4, 5, 6], [4, 5, 6]))
+console.log(checkEquals([4, 7, 6], [4, 5, 6]))
+console.log(checkEquals([4, 7, 6], [4, 6, 7]))
+
