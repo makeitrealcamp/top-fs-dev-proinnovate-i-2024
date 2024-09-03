c53a74ded5bdbea42fe7fd114132a92d63c8a9b0
Johanirus <johan_kyurem@hotmail.com>
2024-09-03 11:08:02 -0500

    fix: solution replace

---
+++ js-exc/johan-pinares/Replace.js
@@ -0,0 +1,6 @@
+const petName = 'Leo'
+const placeholder = '{NAME}'
+const reminderTemplate = '{NAME} is due for another visit. Please call us so we can set up a new appointment. We look forward to seeing you and {NAME} soon.'
+
+const reminder = reminderTemplate.replaceAll(placeholder, petName)
+console.log(reminder)
