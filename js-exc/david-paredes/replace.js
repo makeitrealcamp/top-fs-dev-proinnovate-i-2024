889e63733a656a38f9dcbd8dcfc2b8064ed95347
David Paredes <d.paredes@didepti.com>
2024-09-03 18:53:35 -0500

    fix: solution replace

---
+++ js-exc/david-paredes/replace.js
@@ -0,0 +1,7 @@
+const petName = 'Leo'
+const placeholder = '{NAME}'
+const reminderTemplate = '{NAME} is due for another visit. Please call us so we can set up a new appointment. We look forward to seeing you and {NAME} soon.'
+
+const reminder = reminderTemplate.replaceAll(placeholder, petName)
+
+console.log(reminder)
