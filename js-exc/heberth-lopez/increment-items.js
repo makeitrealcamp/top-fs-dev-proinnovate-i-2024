64d89c94b9853d8f6f62236a9a1d8008030b6205
Heberth López <heberth.lopez.19@gmail.com>
2024-09-03 13:17:59 -0500

    chore: Formateo de código

--- js-exc/heberth-lopez/increment-items.js
+++ js-exc/heberth-lopez/increment-items.js
@@ -1,11 +1,11 @@
 function incrementItems(arr) {
   for (let i = 0; i < arr.length; i++) {
-    arr[i] = arr[i] + 1
+    arr[i] = arr[i] + 1;
   }
 
-  return arr
+  return arr;
 }
 
-console.log(incrementItems([0, 1, 2, 3]))
-console.log(incrementItems([2, 4, 6, 8]))
-console.log(incrementItems([-1, -2, -3, -4]))
+console.log(incrementItems([0, 1, 2, 3]));
+console.log(incrementItems([2, 4, 6, 8]));
+console.log(incrementItems([-1, -2, -3, -4]));
