ccb289be4f40d264ce6854e6428be426cd809c55
Anna Albirena <ana84168@gmail.com>
2024-09-04 13:03:21 -0500

    Bugfix/js exc anna albirena (#61)
    * fix: solution manipulación de arreglos
    
    * fix: solution valor referencia de tipos
    
    * fix: solution aplanando arreglo
    
    * fix: solution clonar arreglo
    
    * fix: solution devolucion precios validos
    
    * fix: solution desmutar arreglo
    
    * fix: solution replace
    
    * fix: solution string

---
+++ js-exc/anna-albirena/valor-referencia-tipos.js
@@ -0,0 +1,10 @@
+// Se agregó un for para comparar los elementos de cada array
+
+function checkEquals(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
