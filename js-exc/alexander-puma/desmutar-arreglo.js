28746cf0b2ca7cb34d9aad7977544e97a1fa9cf4
Alexander <alexpumaprado@gmail.com>
2024-09-04 02:13:11 -0500

    fix: solution desmutar arreglos

---
+++ js-exc/alexander-puma/desmutar-arreglo.js
@@ -0,0 +1,24 @@
+function change(arr, times) {
+
+  const result = arr.slice()
+
+  // Iteración para insertar
+  for (let i = 0; i < result.length; i++) {
+    let j = 1
+    while (j <= times) {
+      if (i >= j && i < result.length - j) {
+        result[i]--
+      }
+      j++
+    }
+  }
+
+  return result
+}
+
+// Pruebas
+const x = [3, 3, 3, 3, 3, 3, 3]
+console.log(JSON.stringify(change(x, 0))) // [3,3,3,3,3,3,3]
+console.log(JSON.stringify(change(x, 1))) // [3,2,2,2,2,2,3]
+console.log(JSON.stringify(change(x, 2))) // [3,2,1,1,1,2,3]
+console.log(JSON.stringify(change(x, 3)))// [3,2,1,0,1,2,3]
