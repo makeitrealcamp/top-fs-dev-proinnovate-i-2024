1ac836942c929e01b6fbdf8cfbeb9c2faae8a85a
Franco <20141674@aloe.ulima.edu.pe>
2024-09-03 22:34:18 -0500

    fix: solution valor vs referencia de tipos

---
+++ js-exc/franco-casanova/checkEquals.js
@@ -0,0 +1,16 @@
+function checkEquals(arr1, arr2) {
+    return arr1.length === arr2.length && arr1.every(function(v, i) { return v === arr2[i] });
+    /*if (arr1.length == arr2.length) {
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }*/
+}
+console.log(checkEquals([1, 2], [1, 3]))
+console.log(checkEquals([1, 2], [1, 2]))
+console.log(checkEquals([4, 5, 6], [4, 5, 6]))
+console.log(checkEquals([4, 7, 6], [4, 5, 6]))
+console.log(checkEquals([4, 7, 6], [4, 6, 7]))
