fdbaf09509de565c4d6f7ab897071fe85f01a6bf
Mikelyto1994 <mikelyto@gmail.com>
2024-09-05 10:51:44 -0500

    feat solution coleccion

--- nodeschool/learyounode/miguel-pariona/cliente.js
+++ nodeschool/learyounode/miguel-pariona/cliente.js
@@ -1,5 +1,5 @@
-// Cargar el módulo 'https' para realizar peticiones HTTP y HTTPS
-const https = require('http');
+// Cargar el módulo 'http' para realizar peticiones HTTP
+const http = require('http');
 
 // Obtener la URL del primer argumento de la línea de comandos
 const url = process.argv[2];
@@ -11,7 +11,7 @@ if (!url) {
 }
 
 // Realizar la petición HTTP GET
-https
+http
   .get(url, (response) => {
     // Establecer la codificación de la respuesta como 'utf8'
     response.setEncoding('utf8');
@@ -33,6 +33,6 @@ https
     });
   })
   .on('error', (err) => {
-    // Manejar cualquier error que ocurra en la solicitud HTTPS
-    console.error('Error en la solicitud HTTPS:', err.message);
+    // Manejar cualquier error que ocurra en la solicitud HTTP
+    console.error('Error en la solicitud HTTP:', err.message);
   });
