04194c08008eda3d200372bf485e762f17871bb3
William Lupaca <wlupacat@gmail.com>
2024-09-06 05:19:27 -0500

    feat: solution http client learnyounode

---
+++ nodeschool/learyounode/william-lupaca/http-client.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const url = process.argv[2];
+
+http.get(url, (response) => {
+  response.setEncoding('utf8');
+
+  response.on('data', (chunk) => {
+    console.log(chunk);
+  });
+});
