c86624593760ee974d3ac6106c9b9a4b2d9f39a9
bryamaranguri <bryam.aranguri.dev@gmail.com>
2024-09-06 17:20:47 -0500

    solution learnyounode juggling async

--- nodeschool/learyounode/bryam-aranguri/http-collect.js
+++ nodeschool/learyounode/bryam-aranguri/http-collect.js
@@ -1,28 +1,25 @@
-const http = require('http'); // Importamos el módulo HTTP
+const http = require('http');
 
-// Guardamos la URL que el usuario pasa como argumento
 const url = process.argv[2];
 
-// Realizamos una solicitud HTTP GET a la URL
+
 http.get(url, (response) => {
-  let fullData = ''; // Variable para acumular todos los fragmentos de datos
+  let fullData = '';
 
-  // Escuchamos el evento 'data', que se dispara cuando recibimos datos
+
   response.on('data', (chunk) => {
-    fullData += chunk; // Acumulamos cada fragmento de datos en fullData
+    fullData += chunk;
   });
 
-  // Escuchamos el evento 'end', que se dispara cuando termina la recepción de datos
   response.on('end', () => {
-    console.log(fullData.length); // Imprimimos la cantidad de caracteres recibidos
-    console.log(fullData);        // Imprimimos el string completo recibido
+    console.log(fullData.length);
+    console.log(fullData);
   });
 
-  // Escuchamos el evento 'error' en caso de que ocurra un problema con la solicitud
   response.on('error', (err) => {
     console.error('Error:', err.message);
   });
 
 }).on('error', (err) => {
-  console.error('Error con la solicitud:', err.message); // Manejo de errores con la solicitud GET
+  console.error('Error con la solicitud:', err.message);
 });
