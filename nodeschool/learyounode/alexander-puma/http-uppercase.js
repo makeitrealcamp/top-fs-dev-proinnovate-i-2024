33c0a3954e8964de1b7674ceec5f9a79dc141b77
Alexander <alexpumaprado@gmail.com>
2024-09-08 19:47:09 -0500

    feat: solution http-uppercase learnyounode

--- nodeschool/learyounode/alexander-puma/http-uppercase.js
+++ nodeschool/learyounode/alexander-puma/http-uppercase.js
@@ -1,20 +1,15 @@
 const http = require('http');
 const map = require('through2-map');
-
-const port = process.argv[2];
-
 const server = http.createServer((req, res) => {
   if (req.method === 'POST') {
-    req
-      .pipe(
-        map((chunk) => {
-          return chunk.toString().toUpperCase();
-        })
-      )
-      .pipe(res);
+    req.pipe(map((chunk) => {
+      return chunk.toString().toUpperCase();
+    })).pipe(res);
   } else {
-    res.end('Envia una petición.\n');
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
+    res.end('Envía una peticion POST\n');
   }
 });
 
+const port = Number(process.argv[2]);
 server.listen(port);
