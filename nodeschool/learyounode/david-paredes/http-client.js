46252e237c8018c89a6d3b6d0ef5ca0426e9cc35
David Paredes <d.paredes@didepti.com>
2024-09-09 22:11:17 -0500

    feat: solution for http-client

---
+++ nodeschool/learyounode/david-paredes/http-client.js
@@ -0,0 +1,10 @@
+'use strict'
+const http = require('http');
+const url = process.argv[2];
+
+http.get(url, response => {
+  response.setEncoding('utf8');
+  response.on('data', data => {
+    console.log(data);
+  })
+})
