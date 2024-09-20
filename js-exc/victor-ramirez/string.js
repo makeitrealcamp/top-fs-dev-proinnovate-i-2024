c7e8697a79012f65be3c7b6e9333e5444848b8de
Victor Manuel Ramirez Lazarte <vicmanurl17@hotmail.com>
2024-09-20 11:45:57 -0500

    fix: solution manipulación de arreglos

---
+++ js-exc/victor-ramirez/string.js
@@ -0,0 +1,7 @@
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// Ejemplo de uso
+console.log(capitalize("hello")); // "Hello"
+console.log(capitalize("world")); // "World"
