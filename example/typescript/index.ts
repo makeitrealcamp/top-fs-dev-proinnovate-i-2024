5f0b98e893e96ab3fcdc6f5c336d3b0b1007f792
khriztianmoreno <khriztianmoreno@gmail.com>
2024-10-04 09:15:19 -0500

    feat: add prisma example

--- example/typescript/index.ts
+++ example/typescript/index.ts
@@ -1,15 +1,66 @@
-type User = {
-  name: string;
-  age: number;
-};
+import { PrismaClient } from '@prisma/client';
 
-function isAdult(user: User): boolean {
-  return user.age >= 18;
+const prisma = new PrismaClient();
+
+async function main() {
+  // Create a new user
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: 'Cristian Moreno',
+  //     email: 'k@lo.com',
+  //   },
+  // });
+  // console.log(user);
+
+  // Get a user
+  // const users = await prisma.user.findMany();
+  // console.log(users);
+
+  // Relationship
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: 'mafe',
+  //     email: 'mafe@gmail.com',
+  //     posts: {
+  //       create: [
+  //         {
+  //           title: 'Hello world',
+  //           content: 'loremp',
+  //           published: true,
+  //         },
+  //         {
+  //           title: 'my second post',
+  //           content: 'this is still a draft',
+  //         },
+  //       ],
+  //     },
+  //   },
+  // });
+  // console.log(user);
+
+  // Include post
+  const usersWithPost = await prisma.user.findMany({
+    include: {
+      posts: {
+        select: {
+          id: true,
+          title: true,
+          content: true,
+        },
+      },
+    },
+  });
+
+  console.dir(usersWithPost, { depth: null });
 }
 
-const justine: User = {
-  name: 'Justine',
-  age: 23,
-};
-
-const isJustineAnAdult = isAdult(justine);
+main()
+  .then(async () => {
+    console.log('Done');
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
